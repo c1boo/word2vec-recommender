2688e6f710ae2eb0efc00aa95e3635634693e595
c1boo <mehmetali.cibo@gmail.com>
2023-12-17 17:16:47 +0300

    New Data and New Word2Vec Approach

--- word2vec_v2.py
+++ word2vec_v2.py
@@ -1,9 +1,11 @@
 import warnings
 import PIL
 import pandas as pd
+import numpy as np
 import nltk
 from nltk.corpus import stopwords
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import RegexpTokenizer
 import re
 from PIL import Image
@@ -71,39 +73,48 @@ google_word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=True)
 google_model = Word2Vec(vector_size=300, window=5, min_count=2, workers=-1)
 google_model.build_vocab(corpus)
 google_model.train(corpus, total_examples=google_model.corpus_count, epochs=5)
+# Building the TF-IDF model and calculating the TF-IDF score
+tfidf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=5, stop_words='english')
+tfidf.fit(df['Cleaned'])
+
+# Getting the words from the TF-IDF model
+tfidf_list = dict(zip(tfidf.get_feature_names_out(), list(tfidf.idf_)))
+
+# TF-IDF words/column names
+tfidf_feature = tfidf.get_feature_names_out()
+
+# Storing the TFIDF Word2Vec embeddings
+tfidf_vectors = []
+line = 0
+
+# For each 'Description'
+for desc in corpus:
+
+    # Word vectors are of zero length (using 300 dimensions)
+    sent_vec = np.zeros(300)
+
+    # Number of words with a valid vector in the 'Description'
+    weight_sum = 0
+
+    # For each word in the 'Description'
+    for word in desc:
+        if word in google_model.wv.key_to_index and word in tfidf_feature:
+            vec = google_model.wv[word]
+            tf_idf = tfidf_list[word] * (desc.count(word) / len(desc))
+            sent_vec += (vec * tf_idf)
+            weight_sum += tf_idf
+    if weight_sum != 0:
+        sent_vec /= weight_sum
+    tfidf_vectors.append(sent_vec)
+    line += 1
 
 
-def vectors(x):
-    # Creating a list for storing the vectors ('Description' into vectors)
-    global word_embeddings
-    word_embeddings = []
-
-    # Reading the each 'Description'
-    for line in df['Cleaned']:
-        avgword2vec = None
-        count = 0
-        for word in line.split():
-            if word in google_model.wv.key_to_index:
-                count += 1
-                if avgword2vec is None:
-                    avgword2vec = google_model.wv[word]
-                else:
-                    avgword2vec = avgword2vec + google_model.wv[word]
-
-        if avgword2vec is not None:
-            avgword2vec = avgword2vec / count
-            word_embeddings.append(avgword2vec)
-
-
-# Recommending the Top 5 similar movies
+# Recommending top 5 similar movies
 def recommendations(movie):
-    # Calling the function vectors
-    vectors(df)
-
     # Finding cosine similarity for the vectors
-    cosine_similarities = cosine_similarity(word_embeddings, word_embeddings)
+    cosine_similarities = cosine_similarity(tfidf_vectors, tfidf_vectors)
 
-    # Taking the Title and Movie Image Link and store in new dataframe called 'movies'
+    # Taking the Title and Image Link and store in new data frame called movies
     movies = df[['Movie', 'ImgLink']]
 
     # Reverse mapping of the index
